f194de1d417f615049f37fe41f392b7e7ed71726
Shivani21-hude <shivani21hude@gmail.com>
2023-02-15 20:54:37 +0530

    [Shivani]-Create a table name as 'employee_payroll'

--- EmployeePaayroll/SQLQuery1.sql
+++ EmployeePaayroll/SQLQuery1.sql
@@ -5,3 +5,10 @@ create database Payroll
 select db_name()
 use Payroll
 
+--Uc2
+create table employee_payroll(
+id int identity(1,1) primary key,
+name varchar(200),
+salary int,
+start_date date
+);
