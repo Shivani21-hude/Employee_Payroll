f8bbc6cb48f893bdbe5f697ae89c2088434ec9f8
Shivani21-hude <shivani21hude@gmail.com>
2023-02-15 21:05:13 +0530

    [Shivani]-Add values to employee_payroll table

--- EmployeePaayroll/SQLQuery1.sql
+++ EmployeePaayroll/SQLQuery1.sql
@@ -12,3 +12,12 @@ name varchar(200),
 salary int,
 start_date date
 );
+
+--Uc3
+select * from INFORMATION_SCHEMA.columns where table_name='employee'
+insert into employee_payroll values ('Shivani',20000,'2018-02-21')
+insert into employee_payroll (salary,start_date,name) values (2000,'2023-02-03','Trisha')
+insert into employee_payroll values
+('jasmin',34567,'2019-09-08'),
+('sima',9877,'2012-06-05')
+
