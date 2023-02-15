32ab3f1ca225077f122b2ba8a565f6ba55915eee
Shivani21-hude <shivani21hude@gmail.com>
2023-02-15 21:31:15 +0530

    [Shivani]-To add Gender to emplyee_payroll table and update rows

--- EmployeePaayroll/SQLQuery1.sql
+++ EmployeePaayroll/SQLQuery1.sql
@@ -20,6 +20,8 @@ insert into employee_payroll (salary,start_date,name) values (2000,'2023-02-03',
 insert into employee_payroll values
 ('jasmin',34567,'2019-09-08'),
 ('sima',9877,'2012-06-05')
+insert into employee_payroll values ('Vrutik',3245,'2021-08-12'),
+('shubh',4321,'2019-09-24')
 
 --Uc4
 
@@ -28,3 +30,9 @@ select * from employee_payroll
 --Uc5
 select salary from employee_payroll where name ='Shivani'
 select * from employee_payroll where start_date between '2019-02-22' and '2023-04-21'
+
+--Uc6
+alter table employee_payroll add Gender char(1)
+update employee_payroll set Gender ='F' where name='Shivani' or name='sima'
+update employee_payroll set gender ='F' where id in (2,3)
+update employee_payroll set gender ='M' where id in (5,6)
